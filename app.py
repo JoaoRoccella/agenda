15900aa4cf91e44739742984300414a16263ab1d
JoaoRoccella <roccella@gmail.com>
2026-02-09 16:47:14 -0300

    feat: crud concluído e estilização da lista de tarefas

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def agenda():
         tarefa.salvar_tarefa()
 
     tarefas = Tarefa.obter_tarefas()
-    return render_template('agenda.html', titulo='Agenda', tarefas=tarefas)
+    return render_template('agenda.html', titulo='Nova tarefa', tarefas=tarefas)
 
 @app.route('/delete/<int:idTarefa>')
 def delete(idTarefa):
@@ -26,6 +26,21 @@ def delete(idTarefa):
     tarefa.excluir_tarefa()
     # return render_template('agenda.html', titulo="Agenda", tarefas=tarefas)
     return redirect(url_for('agenda'))
+
+@app.route('/update/<int:idTarefa>', methods = ['GET', 'POST'])
+def update(idTarefa):
+    
+    if request.method == 'POST':
+        titulo = request.form['titulo-tarefa']
+        data = request.form['data-conclusao']
+        tarefa = Tarefa(titulo, data, idTarefa)
+        tarefa.atualizar_tarefa()
+        return redirect(url_for('agenda')) # early return
+
+    tarefas = Tarefa.obter_tarefas()
+    tarefa_selecionada = Tarefa.id(idTarefa) # seleção da tarefa que será editada
+
+    return render_template('agenda.html', titulo=f'Editando a tarefa ID: {idTarefa}', tarefas=tarefas, tarefa_selecionada=tarefa_selecionada)
 
 @app.route('/ola')
 def ola_mundo():
